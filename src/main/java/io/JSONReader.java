cde852ec8ff40ca0964950321f3dfa328299cd92
samuseb <samuseb@gmail.com>
2022-05-19 00:15:53 +0200

    JSONReader implemented

--- src/main/java/io/JSONReader.java
+++ src/main/java/io/JSONReader.java
@@ -1,12 +1,61 @@
 package io;
 
+import price.Price;
 import tseries.TimeSeries;
-
 import java.io.IOException;
+import java.io.FileReader;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
 
 public class JSONReader {
 
-    
+    public JSONReader() {}
+
+    public TimeSeries read(String inputJSON) throws IOException {
+
+        JSONParser jsonParser = new JSONParser();
+        TimeSeries tseries = new TimeSeries();
+        TreeMap<LocalDateTime, Price> tmap = new TreeMap<>();
+
+        try {
+            JSONArray jsonArray = (JSONArray) jsonParser.parse(new FileReader(inputJSON));
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm:ss'.000000000Z'", Locale.US);
+
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+
+                String stringTime = (String)jsonObject.get("time");
+                LocalDateTime time = LocalDateTime.parse(stringTime, formatter);
+
+                JSONObject priceJsonObject = (JSONObject)jsonObject.get("mid");
+
+                Price price = new Price();
+                price.setOpen ( Double.parseDouble( (String) priceJsonObject.get("o")) );
+                price.setHigh ( Double.parseDouble( (String) priceJsonObject.get("h")) );
+                price.setLow  ( Double.parseDouble( (String) priceJsonObject.get("l")) );
+                price.setClose( Double.parseDouble( (String) priceJsonObject.get("c")) );
+
+                tmap.put(time, price);
+
+            }
+
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        tseries.setValues(tmap);
+        return tseries;
+    }
+
 
 
 
